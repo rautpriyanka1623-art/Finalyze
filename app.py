3b5629486c4f5bd243774453790b5f23fe077b23
Priyanka Raut S <rautpriyanka1623@gmail.com>
2026-02-23 19:02:50 +0530

    Day2:Added form handling and expense display

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+expenses = [] # this will store all expenses
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    if request.method == 'POST':
+        amount = request.form['amount']
+        category = request.form['category']
+        description = request.form['description']
+
+        expense = {
+            'amount': amount,
+            'category': category,
+            'description': description
+        }
+
+        expenses.append(expense)
+
+    return render_template('index.html', expenses=expenses)
 
 if __name__ == '__main__':
     app.run(debug=True)
